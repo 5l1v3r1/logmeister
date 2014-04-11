de70ed6d6bfcda15a719390d328ac696e99efc14
Brady Sullivan <brady.sullivan@iwsinc.com>
2014-04-11 13:32:01 -0700

    Add event functionality with tests.

---
+++ event.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "errors"
+)
+
+
+const EmptyActionError = "Action must not be empty."
+const EmptyTargetError = "Target must not be empty."
+const EmptyResultError = "Result must not be empty."
+
+
+type Event struct {
+	Action string
+	Target string
+	Result string
+  Data string
+}
+
+
+func NewEvent(action, target, result, data string) (e *Event, err error) {
+  if action == "" {
+    err = errors.New(EmptyActionError)
+  } else if target == "" {
+    err = errors.New(EmptyTargetError)
+  } else if result == "" {
+    err = errors.New(EmptyResultError)
+  } else {
+    e = &Event{action, target, result, data}
+  }
+  return
+}
---
+++ event_test.go
@@ -0,0 +1,62 @@
+package main
+
+import "testing"
+
+const TestAction = "ping"
+const TestTarget = "test_target"
+const TestResult = "200"
+const TestString = "Winter is Coming"
+
+func TestNewEvent(t *testing.T) {
+	test_data := TestString
+	e, err := NewEvent(TestAction, TestTarget, TestResult, test_data)
+	if err != nil {
+		t.Fatalf("Error creating new event: %v", err)
+	}
+	if e.Action != TestAction {
+		t.Errorf("Action improperly set: %v", e.Action)
+	}
+	if e.Target != TestTarget {
+		t.Errorf("Target improperly set: %v", e.Target)
+	}
+	if e.Result != TestResult {
+		t.Errorf("Result improperly set: %v", e.Result)
+	}
+	if e.Data != TestString {
+		t.Errorf("Data improperly set: %v", e.Data)
+	}
+}
+
+func TestAllEmptyEvent(t *testing.T) {
+	e, err := NewEvent("", "", "", "")
+	EmptyTestHelper(t, e, err, EmptyActionError)
+}
+
+func TestEmptyAction(t *testing.T) {
+	e, err := NewEvent("", "asdf", "asdf", "asdf")
+	EmptyTestHelper(t, e, err, EmptyActionError)
+}
+
+func TestEmptyTarget(t *testing.T) {
+	e, err := NewEvent("test", "", "asdf", "asdf")
+	EmptyTestHelper(t, e, err, EmptyTargetError)
+}
+
+func TestEmptyResult(t *testing.T) {
+	e, err := NewEvent("test", "asdf", "", "asdf")
+	EmptyTestHelper(t, e, err, EmptyResultError)
+}
+
+func EmptyTestHelper(t *testing.T, e *Event, err error, error_string string) {
+	if e != nil {
+		t.Errorf("e2: Should have returned nil.")
+	}
+	if err == nil {
+		t.Errorf("Should have recieved error.")
+	}
+	if err != nil {
+		if err.Error() != error_string {
+			t.Errorf("Should have recieved %s, instead recieved: %v", error_string, err)
+		}
+	}
+}
